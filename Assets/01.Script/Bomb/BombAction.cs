cdaaf727a621d3fcb1347a7fc661a4e5a27173b5
LeeJongHyuk <jjong7477@gmail.com>
2025-05-27 20:09:15 +0900

    Fix : Bomb Scripts

--- Assets/01.Script/Bomb/BombAction.cs
+++ Assets/01.Script/Bomb/BombAction.cs
@@ -1,48 +1,65 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
+using System.Diagnostics;
+using TMPro;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
-public class BombObject : MonoBehaviour
+public class BombAction : MonoBehaviour
 {
-    public BombData data;
     private Collider _collider;
+    private BombStatus _status;
+    protected BombData _data;
+    
 
-    private void OnEnable()
+    private void Awake()
     {
         _collider = this.gameObject.GetComponent<Collider>();
         _collider.transform.localScale = Vector3.zero;
-        PlacedType();
     }
-    
-    //폭탄 종류 확인 메서드
-    private void PlacedType()
+
+    private void Start()
     {
-        switch (data.bombType)
-        {
-            case BombType.Bound:
-            case BombType.Demolition:
-            case BombType.Freeze:
-            case BombType.Laser:
-                StartCoroutine(Explode());
-                break;
-            case BombType.Portal:
-                Install();
-                break;
-        }
+        StartCoroutine(Explode());
     }
-    
-    //폭탄 폭발 메서드
+
+    private void Init()
+    {
+        _status = this.gameObject.GetComponent<BombStatus>();
+        _data = _status.data;
+    }
+
     public IEnumerator Explode()
     {
-        yield return new WaitForSeconds(data.explodeTime);
+        Init();
+        yield return new WaitForSeconds(_data.explodeTime);
         float time = 0f;
         while (time < 1f)
         {
-            _collider.transform.localScale = Vector3.Lerp(Vector3.one * data.explodeRange, Vector3.zero, time);
+            _collider.transform.localScale = Vector3.Lerp(Vector3.one * _data.explodeRange, Vector3.zero, time);
             time += Time.deltaTime;
             yield return null;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_data.bombType == BombType.Bound)
+        {
+            Bound(other);
+        }
+        else if (_data.bombType == BombType.Demolition)
+        {
+            Demolition(other);
+        }
+        else if (_data.bombType == BombType.Laser)
+        {
+            Laser(other);
+        }
+        else if (_data.bombType == BombType.Freeze)
+        {
+            StartCoroutine(Freeze(other));
         }
     }
 
@@ -55,7 +72,7 @@ public class BombObject : MonoBehaviour
         else if (target.CompareTag("Player"))
         {
             var rigid = target.GetComponent<Rigidbody>();
-            rigid.AddForce(Vector3.up * data.explodePower, ForceMode.Impulse);
+            rigid.AddForce(Vector3.up * _data.explodePower, ForceMode.Impulse);
         }
     }
 
@@ -69,6 +86,23 @@ public class BombObject : MonoBehaviour
         {
             target.gameObject.SetActive(false);
         }
+    }
+
+    private void Laser(Collider target)
+    {
+        if (target == null)
+        {
+            Debug.Log("Target is null");
+        }
+        // else if (target.CompareTag("Interactable"))
+        // {
+        //     //대강 요런 느낌..?
+        //     //IInteractable interactable = target.GetComponent<IInteractable>();
+        //     // if (interactable != null)
+        //     // {
+        //     //     interactable.Interact();
+        //     // }
+        // }
     }
 
     private IEnumerator Freeze(Collider target)
@@ -86,7 +120,7 @@ public class BombObject : MonoBehaviour
             rigid.velocity = Vector3.zero;
             anim.speed = 0f;
             
-            yield return new WaitForSeconds(data.freezeDuration);
+            yield return new WaitForSeconds(_data.freezeDuration);
             
             float time = 0f;
             while (time < 1f)
@@ -98,34 +132,4 @@ public class BombObject : MonoBehaviour
             }
         }
     }
-    
-    private void Install()
-    {
-        var target = FindObjectsOfType<BombObject>();
-        int count = 0;
-        
-        for (int i = 0; i < target.Length; i++)
-        {
-            Debug.Log(target[i].data.bombType);
-            
-            if (target[i].data.bombType == BombType.Portal)
-            {
-                count++;
-            }
-        }
-
-        if (count == 1)
-        {
-            Debug.Log("포탈 준비 중");
-        }
-        else if (count == 2)
-        {
-            Debug.Log("포탈 설치 완료");
-        }
-    }
-    
-    /* 폭탄을 설치
-     * 폭탄 폭발
-     * 폭탄 종류에 따라 효과 발생
-     */
 }
---
+++ Assets/01.Script/Bomb/BombStatus.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BombStatus : MonoBehaviour
+{
+    public BombData data;
+    private BombAction _action;
+
+    private void Awake()
+    {
+        _action = this.gameObject.GetComponent<BombAction>();
+    }
+
+    public void PowerUp(int power)
+    {
+        data.explodePower += power;
+    }
+
+    public void RangeUp(int range)
+    {
+        data.explodeRange += range;
+    }
+
+    public void CoolTimeDown(float time)
+    {
+        if (time < data.bombCooldown)
+        {
+            data.bombCooldown -= time;
+        }
+        else
+        {
+            data.bombCooldown = 0f;
+        }
+    }
+}
