2bce86c406fc40948680b61667752afd9aca9a20
TaeYoungTOF <113530625+TaeYoungTOF@users.noreply.github.com>
2025-05-29 15:25:06 +0900

    refactor: StageManager의 기능 Stage로 이관
    - 게임플레이와 관련된 StageManager.cs의 부분을 Stage.cs로 이관

--- Assets/01.Script/Manager/StageManager.cs
+++ Assets/01.Script/Manager/StageManager.cs
@@ -19,63 +19,18 @@ public struct StageData
 
 public class StageManager : MonoBehaviour
 {
-
-    public UIManager uiManager;
-    public int usedBombCount;
-
     public const int stageCount = 3;
     public List<StageData> stageDataList = new List<StageData>();
     public int curStageId;
-
-    //Player player;
-
-    public float ElapsedTime { get; private set; }
-    public bool IsCleared { get; private set; }
-
-    private void Awake()
-    {
-        IsCleared = false;
-    }
-
 
     private void Start()
     {
         NewStageData();
     }
 
-    private void Update()
-    {
-        if (!IsCleared)
-        {
-            ElapsedTime += Time.deltaTime;
-        }
-    }
-
-    public void InitStage()
-    {
-        usedBombCount = 0;
-        ElapsedTime = 0;
-        IsCleared = false;
-        Cursor.lockState = CursorLockMode.Locked;
-    }
-
-
-    public void ClearStage()
-    {
-        IsCleared = true;
-        Cursor.lockState = CursorLockMode.None;
-        
-        // TODO : 클리어 시간과 사용 폭탄 수 uiManager에 셋팅
-        // ShowGameClearPanel에 파라미터를 추가하거나 해도 좋을 것 같습니다
-        // 사용 폭탄 갯수 플레이어 말고 스테이지 매니저가 들고 있는 것도 좋을 것 같아요
-        // 경과 시간은 Update에도 적혀있는 ElapsedTime 참고
-        
-        uiManager.ShowGameClearPanel();
-    }
     public void NewStageData()
     {
         stageDataList.Clear();
-
 
         for (int i = 0; i < stageCount; i++)
         {
@@ -146,7 +101,5 @@ public class StageManager : MonoBehaviour
         }
         SaveStageData();
         LoadStage(curStageId);
-    }
-
-    
+    }    
 }
